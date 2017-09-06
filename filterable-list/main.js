f6e4d4254af937e81d13c4cd5d17360eb0954f6d
Jorge Cuevas Quezada <jcuevas@segurospepin.com>
2017-09-06 13:53:40 -0400

    search match fixed

--- filterable-list/main.js
+++ filterable-list/main.js
@@ -1,19 +1,12 @@
-let filterInput = document.getElementById('filterInput');
-filterInput.addEventListener('keyup', filterByName);
-
-function filterByName(e) {
-    console.log('value right there' + e.target.value);
-}
-
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let contactList = document.querySelector('ul.collection');
+let contactList = document.getElementById('names');
 let header = document.getElementById('header');
 let item = document.getElementById('item');
 
 
 
-function getAvatar(name){
-    return (name.trim().length % 2 > 0) ? "img/avatar.png" : "img/avatar2.png" ;
+function getAvatar(name) {
+    return (name.trim().length % 2 > 0) ? "img/avatar.png" : "img/avatar2.png";
 }
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -26,7 +19,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
             headerClone.textContent = alphabet[x];
             contactList.appendChild(headerClone);
 
-            users.forEach(function (user) {
+            users.forEach((user) =>{
                 if (user.name.startsWith(alphabet[x])) {
                     itemClone = item.cloneNode(true);
                     itemClone.innerHTML = `<img src="${getAvatar(user.name)}" alt="" class="circle" />
@@ -42,3 +35,27 @@ fetch('https://jsonplaceholder.typicode.com/users')
         contactList.removeChild(item);
     })
     .catch((err) => console.log(err));
+
+
+let filterInput = document.getElementById('filterInput');
+filterInput.addEventListener('keyup', filterByName);
+
+function filterByName(e) {
+    console.log('up up up')
+    let filterValue = filterInput.value.toUpperCase();
+    //get ul element
+    let ul = document.getElementById('names');
+
+    //get lis form ul
+    let li = ul.querySelectorAll('li.collection-item');
+
+    for (let x = 0; li.length > x; x++) {
+        let a = li[x].getElementsByTagName('a')[0];
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[x].style.display = '';
+        } else {
+            li[x].style.display = 'none';
+        }
+    }
+
+}
